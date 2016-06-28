ddb1ffbbfd463bdd35090419d3ddcc30caba5bf0
Chander Inguva <chander.inguva@gmail.com>
2016-06-28 09:00:38 -0500

    Use LIKE instead of IN for better results

--- ProjectKey-Inactive-Userrole-Query.sql
+++ ProjectKey-Inactive-Userrole-Query.sql
@@ -1,3 +1,6 @@
 SELECT pkey FROM project WHERE ID IN (SELECT PID from projectroleactor where PID in 
-(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER IN ('DSummers'))
+(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER LIKE '%nbaker%')
  AND ROLETYPE <> 'atlassian-group-role-actor')
+ 
+ 
+ 
