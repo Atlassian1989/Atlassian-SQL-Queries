87136f2deef30beffd938e670ea5f4f0526fac9d
Chander Inguva <chander.inguva@gmail.com>
2016-07-05 13:57:21 -0500

    Updated Jul 5th 2016

--- Wiki-Space-Perm-Inactive-User-Cleanup.sql
+++ Wiki-Space-Perm-Inactive-User-Cleanup.sql
@@ -1,5 +1,6 @@
+/* Checked Inactive User relationships with Confluence Spaces as of 7/5/2016 */
 SELECT sp.SPACEID,s.SPACEKEY,s.SPACENAME,um.username FROM SPACEPERMISSIONS sp JOIN user_mapping  um ON
 um.user_key = sp.PERMUSERNAME
 JOIN SPACES s ON 
 sp.SPACEID = s.SPACEID
-WHERE sp.PERMUSERNAME IS NOT NULL AND um.username LIKE '%PCarrell'
+WHERE sp.PERMUSERNAME IS NOT NULL AND um.username LIKE '%TMyers'
