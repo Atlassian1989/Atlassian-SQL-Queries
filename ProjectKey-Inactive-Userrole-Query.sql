e81fd2cdb62d9813ec13c22c2a2ec418989f40a3
Chander Inguva <chander.inguva@gmail.com>
2016-07-11 13:34:24 -0500

    Updated till Jul 8

--- ProjectKey-Inactive-Userrole-Query.sql
+++ ProjectKey-Inactive-Userrole-Query.sql
@@ -1,8 +1,7 @@
-/* Checked Inactive User relationships with JIRA Projects as of 7/5/2016 */
+/* Checked Inactive User relationships with JIRA Projects as of 7/8/2016 */
 SELECT pkey FROM project WHERE ID IN (SELECT PID from projectroleactor where PID in 
-(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER LIKE '%TMyers%')
+(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER LIKE '%pwinter%')
  AND ROLETYPE <> 'atlassian-group-role-actor')
- 
  
  
  
