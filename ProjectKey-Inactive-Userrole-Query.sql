ff336dafc281f9709738fbfe364dcd169a0463c4
Chander Inguva <chander.inguva@gmail.com>
2016-08-09 10:00:08 -0500

    Updated Aug 9th 2016

--- ProjectKey-Inactive-Userrole-Query.sql
+++ ProjectKey-Inactive-Userrole-Query.sql
@@ -1,3 +1,3 @@
 SELECT pkey FROM project WHERE ID IN (SELECT PID from projectroleactor where PID in 
-(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER LIKE '%Ambarnikova%')
+(select SOURCE_NODE_ID from nodeassociation where ROLETYPEPARAMETER LIKE '%mgomez%')
  AND ROLETYPE <> 'atlassian-group-role-actor')
