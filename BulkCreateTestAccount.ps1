0d9e2b79a812e87b1e961582a3c6ae89bb6dae17
Chander Inguva <cinguva@users.noreply.github.com>
2019-05-13 11:31:13 -0500

    Add files via upload

---
+++ BulkCreateTestAccount.ps1
@@ -0,0 +1,9 @@
+﻿#Install-Module LithnetRMA
+#Import-Module LithnetRMA
+
+$obj = New-Resource -ObjectType Person
+$obj.AccountName ="MIMTestAccount1"
+$obj.Domain ="gogo"
+$obj.DisplayName = "MIM Test Account 1"
+$obj.Manager = "cinguva"
+Save-Resource $obj
---
+++ SeatingLocationMovementScript.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module LithnetRMA;
+
+Get-Credential | Set-ResourceManagementClient -BaseAddress http://CAZ-VMW-MIM-10.gogo.local:5725;
+
+
+$file = import-csv -Path C:\SeatingMovement.csv
+$file.Count
+
+
+foreach ($row in $file) 
+{
+
+    
+    $obj = Get-Resource -ObjectType Person -AttributeValuePairs @{AccountName = $row.'AccountName'; Domain="gogo"}
+    
+    Write-Host "Working on Account" + $obj.AccountName
+
+    $obj.SeatingLocation = $row.'New Seat'
+    Save-Resource $obj
+
+    Write-Host "Updated Seating Location for: " $obj.AccountName "to" $row.'New Seat' 
+        
+     
+     
+
+}
+
+
+
